15df05492d5678c63739cd34ba2a69eafd7040e1
Gururaj Karatagi <gururajkaratagi@gmail.com>
2024-08-10 17:16:13 +0530

    Final commit

---
+++ app/src/main/java/com/example/quiz_app/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.quiz_app;
+
+public class QuestionAnswer {
+
+    public static String question []={
+            "Which country has beautiful Viewpoints?",
+            "Who is best trader?",
+            "who is Pappu?",
+            "who is the founder of SpaceX?",
+            "In the given options, which is the Example of System Software?"
+    };
+    public static String choices [][]={
+            {"Russia" , "China" , "India" , "Nepal"},
+            {"Arun" , "Chetan" , "Edison" , "None of the above"},
+            {"Rahul gandhi" , "yashwanth" , "karthik" , "vijay"},
+            {"Jeff Bezos" , "Elon Musk" , "Steve Jobs" , "Bill Gates"},
+            {"Windows" , "Linux" , "MacOS" , "All of the above"}
+    };
+    public static String correctAnswers []={
+            "Russia" ,
+            "Chetan",
+            "Rahul gandhi",
+            "Elon Musk",
+            "All of the above"};
+}
